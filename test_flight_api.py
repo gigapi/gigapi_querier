9d33acb7cc30c416b5a16b3761477f9e407a6e0b
lmangani <>
2025-04-28 22:47:08 +0000

    local

--- test_flight_api.py
+++ test_flight_api.py
@@ -3,6 +3,6 @@ from flightsql import connect, FlightSQLClient
 client = FlightSQLClient(host='localhost',port=8082,insecure=True,metadata={'bucket':'hep'})
 conn = connect(client)
 cursor = conn.cursor()
-cursor.execute('SELECT 1')
+cursor.execute('SELECT 1, version()')
 # print("columns:", cursor.description)
 print("rows:", [r for r in cursor])
