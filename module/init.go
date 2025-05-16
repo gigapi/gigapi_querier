32cfb5f75aad7d3d128bb5e5b1cbd20b929330cd
akvlad <akvlad90@gmail.com>
2025-05-16 16:36:14 +0300

    remove ui from the querier module init

--- module/init.go
+++ module/init.go
@@ -4,8 +4,6 @@ import (
 	"github.com/gigapi/gigapi-config/config"
 	"github.com/gigapi/gigapi-querier/querier"
 	"github.com/gigapi/gigapi/v2/modules"
-	"github.com/spf13/afero"
-	"io/fs"
 	"net/http"
 )
 
@@ -32,22 +30,6 @@ func Init(api modules.Api) {
 		Path:    "/query",
 		Methods: []string{"GET", "POST", "OPTIONS"},
 		Handler: WithNoError(server.HandleQuery),
-	})
-	api.RegisterRoute(&modules.Route{
-		Path:    "/",
-		Methods: []string{"GET", "OPTIONS"},
-		Handler: WithNoError(server.HandleUI),
-	})
-	afero.Walk(server.UIFS, "/", func(path string, d fs.FileInfo, err error) error {
-		if d == nil || len(path) <= 5 {
-			return nil
-		}
-		api.RegisterRoute(&modules.Route{
-			Path:    path[len("/dist"):],
-			Methods: []string{"GET"},
-			Handler: WithNoError(server.HandleUI),
-		})
-		return nil
 	})
 }
 
